149992b5a7dd9c02e4f7b738da28a44a41d8dc90
vianney diris <vianney.diris@gmail.com>
2019-01-15 13:39:46 +0100

    Correction pour affichage en JSON

--- src/main/java/com/lpdm/msstock/controller/StockController.java
+++ src/main/java/com/lpdm/msstock/controller/StockController.java
@@ -3,6 +3,7 @@ package com.lpdm.msstock.controller;
 import com.lpdm.msstock.dao.StockDao;
 import com.lpdm.msstock.entity.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,21 +14,21 @@ public class StockController {
     @Autowired
     private StockDao stockDao;
 
-    @GetMapping(value = "/stocks")
+    @GetMapping(value = "/stocks", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Stock> listStock(){
         List<Stock> list = stockDao.findAll();
 
         return list;
     }
 
-    @GetMapping(value="/stocks/{id}")
+    @GetMapping(value="/stocks/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Stock findStock(@PathVariable int id){
         Stock stock = stockDao.findById(id);
 
         return stock;
     }
 
-    @PostMapping(value = "/stocks")
+    @PostMapping(value = "/stocks", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Stock addStock(@RequestBody Stock stock){
 
         Stock stockAdded = stockDao.save(stock);
@@ -39,20 +40,20 @@ public class StockController {
         return stockAdded;
     }
 
-    @DeleteMapping(value="/stocks/{id}")
+    @DeleteMapping(value="/stocks/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public void deleteStock(@PathVariable int id){
         stockDao.deleteById(id);
     }
 
-    @PutMapping(value="/stocks")
+    @PutMapping(value="/stocks", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Stock updateStock(@RequestBody Stock stock){
         Stock stockUpdate = stockDao.save(stock);
 
         return stockUpdate;
     }
 
-    @GetMapping(value = "/stocks/productor/{id}")
-    public List<Stock> listStockByProductor(@PathVariable int id){
+    @GetMapping(value = "/stocks/producer/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Stock> listStockByProducer(@PathVariable int id){
         List<Stock> list = stockDao.findStockByProductId(id);
 
         return list;
