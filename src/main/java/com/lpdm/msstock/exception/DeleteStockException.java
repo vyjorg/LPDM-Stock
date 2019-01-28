799ac7b2f45a28f37995e6708a10a7242087bdc0
vianney diris <vianney.diris@gmail.com>
2019-01-28 14:10:18 +0100

    Correction du httpstatus

--- src/main/java/com/lpdm/msstock/exception/DeleteStockException.java
+++ src/main/java/com/lpdm/msstock/exception/DeleteStockException.java
@@ -3,7 +3,7 @@ package com.lpdm.msstock.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_GATEWAY)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class DeleteStockException extends RuntimeException{
     public DeleteStockException(String s) {
         super(s);
--- src/main/java/com/lpdm/msstock/exception/StockNotFound.java
+++ src/main/java/com/lpdm/msstock/exception/StockNotFound.java
@@ -2,7 +2,7 @@ package com.lpdm.msstock.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class StockNotFound extends RuntimeException{
     public StockNotFound(String s) {
         super(s);
