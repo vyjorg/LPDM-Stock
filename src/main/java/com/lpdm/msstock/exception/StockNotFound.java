bdb6c5ccd700290ddb8d90d19e05fa0a2bb8b44c
vianney diris <vianney.diris@gmail.com>
2019-01-28 14:17:25 +0100

    Correction de la gestion des erreurs

--- src/main/java/com/lpdm/msstock/exception/StockNotFound.java
+++ src/main/java/com/lpdm/msstock/exception/StockNotFound.java
@@ -2,7 +2,7 @@ package com.lpdm.msstock.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class StockNotFound extends RuntimeException{
     public StockNotFound(String s) {
         super(s);
